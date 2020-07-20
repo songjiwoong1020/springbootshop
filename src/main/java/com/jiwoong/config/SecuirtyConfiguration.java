25366a5b2d33e857c7d99f065da3e6b8e2199b75
songjiwoong1020 <songjiwoong1020@gmail.com>
2020-07-20 18:39:46 +0900

    시큐리티 적용중...

--- src/main/java/com/jiwoong/config/SecuirtyConfiguration.java
+++ src/main/java/com/jiwoong/config/SecuirtyConfiguration.java
@@ -1,10 +1,12 @@
 package com.jiwoong.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -19,13 +21,25 @@ public class SecuirtyConfiguration extends WebSecurityConfigurerAdapter{
 		.and()
 			.csrf()//csrf에 대해서는 더 공부가 필요..
 		.and()
+			.httpBasic()
+		.and()
 			.formLogin()
 		.and()
-			.logout();
+			.logout()
+		.and()
+			.exceptionHandling().accessDeniedPage("403"); //403페이지 처리
 	}
 	
 	@Override
 	public void configure(WebSecurity web) throws Exception {
 		web.ignoring().antMatchers("/admin/login");
 	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	}
+	
+	
+	
+	
 }
